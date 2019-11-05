ea3e0f04dd3e0c35404248b9e0abf6cde4e3661c
Terra Williams <terraw9@gmail.com>
2019-11-04 19:20:31 -0800

    Tied tab content to different xaml files

---
+++ ChaoticCreation/NpcGenerator/NpcGenerator.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChaoticCreation.NpcGenerator
+{
+    /// <summary>
+    /// Interaction logic for NpcGenerator.xaml
+    /// </summary>
+    public partial class NpcGenerator : UserControl
+    {
+        public NpcGenerator()
+        {
+            InitializeComponent();
+        }
+    }
+}
