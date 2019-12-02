3cad7410ee603efe1907bd25e5836d358f383f8c
Leah <lkramer37@nevada.unr.edu>
2019-12-02 12:25:56 -0800

    Adding sqlDatabase

---
+++ ChaoticCreation/sqlDatabase/npcQuery.sql
@@ -0,0 +1,17 @@
+SELECT
+	occ.OccName,
+	q.QualityDescription,
+	i.ItemDescription,
+	a.ActionDescription
+FROM
+	Occupation occ
+LEFT JOIN Quality q ON q.QualityID == occ.OccQuality
+INNER JOIN OccItems oi ON oi.OccID == occ.OccID
+INNER JOIN Items i ON i.ItemID == oi.ItemID
+INNER JOIN OccAction oa ON oa.OccID == occ.OccID
+INNER JOIN Actions a ON a.ActionID == oa.ActionID
+
+ORDER BY
+	RANDOM()
+LIMIT 1;
+
