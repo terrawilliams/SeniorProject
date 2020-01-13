3cf4cf2bdb6fd9d38075ca9f9bd3ab668af8b8a0
Terra Williams <terraw9@gmail.com>
2020-01-13 14:08:28 -0800

    Selected NPC or Location under recent Creations is displayed on home page

--- ChaoticCreation/HomeTab/HomeTabModel.cs
+++ ChaoticCreation/HomeTab/HomeTabModel.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChaoticCreation.HomeTab
 {
-    class HomeTabModel
+    class HomeTabModel : INotifyPropertyChanged
     {
         #region Members
-        private string applicationDescription;
+        private Creation selectedCreation;
+        private string selectedCreationContent = string.Empty;
+
+        public event PropertyChangedEventHandler PropertyChanged;
         #endregion
 
         #region Getters and Setters
@@ -19,18 +23,42 @@ namespace ChaoticCreation.HomeTab
             get { return RecentCreations.MostRecentCreations; }
         }
 
-        public string ApplicationDescription
+        public Creation SelectedRecentCreation
         {
-            get { return applicationDescription; }
-            set { applicationDescription = value; }
+            set 
+            { 
+                selectedCreation = value;
+                OnPropertyChanged("SelectedRecentCreation");
+
+                if(selectedCreation.Type == GeneratorTypesEnum.NPC || selectedCreation.Type == GeneratorTypesEnum.Location)
+                {
+                    selectedCreationContent = selectedCreation.Generation["description"];
+                    OnPropertyChanged("SelectedCreationContent");
+                }
+            }
+            get { return selectedCreation; }
+        }
+
+        public string SelectedCreationContent
+        {
+            get { return selectedCreationContent; }
         }
         #endregion
 
         #region Constructor
         public HomeTabModel()
         {
-            applicationDescription = "Chaotic Creations is a website made for Dungeon Masters of the fifth edition of Dungeons and Dragons...";
+
+        }
+        #endregion
+
+        #region Methods
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
         }
         #endregion
     }
+
 }
