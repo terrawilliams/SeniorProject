c833836ec989deaa62d6abcf2f049a57b2e584bd
Christopher Lewis <christopher_le1@nevada.unr.edu>
2020-02-25 08:17:21 -0800

    Searchin' and Rollin'

--- ChaoticCreation/RandomTables/RandomTables.xaml.cs
+++ ChaoticCreation/RandomTables/RandomTables.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using ChaoticCreation.RandomTables;
 
 namespace ChaoticCreation.RandomTables
 {
@@ -20,6 +15,11 @@ namespace ChaoticCreation.RandomTables
     /// </summary>
     public partial class RandomTables : UserControl
     {
+        private TreeView RANDOM_TABLES_GLOBAL = new TreeView();
+        List<RandomTableCategory> table = new List<RandomTableCategory>();
+        List<RandomTableCategory> x = new List<RandomTableCategory>();
+        bool firstInitialize = false;
+
         RandomGeneratorModel randomGeneratorModel = new RandomGeneratorModel();
 
         public RandomTables()
@@ -33,7 +33,7 @@ namespace ChaoticCreation.RandomTables
             }
             
         }
-
+        
         private void GenerateButton_Click(object sender, RoutedEventArgs e)
         {
             randomGeneratorModel.GenerateRandomTableSelection();
@@ -41,12 +41,123 @@ namespace ChaoticCreation.RandomTables
 
         private void RollButton_Click(object sender, RoutedEventArgs e)
         {
-            randomGeneratorModel.RollDie();
+            try
+            {
+                var table = TableList.ItemsSource as IList<KeyValuePair<string, string>>;
+
+                if (table.Count != 0)
+                {
+                    DiceSize.Text = table.Count.ToString();
+
+                }
+
+                int result = Int32.Parse(DiceSize.Text);
+
+                result = randomGeneratorModel.RollDie(result);
+                dieFace.Text = result.ToString();
+
+                try
+                {
+
+                    if ((result % table.Count) == 0)
+                    {
+                        result = table.Count - 1;
+                    }
+                    else
+                    {
+                        result = result % table.Count - 1;
+                    }
+                    for (int i = 0; i <= result; i++)
+                    {
+                        if (i == result)
+                        {
+                            Console.WriteLine(table[i].Value);
+                            TableList.SelectedItem = table[i];
+                        }
+                    }
+
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Divide by Zero (This is caused by not selecting a table in random table, so generally okay)");
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine("Move Next failed (line 52 RandomTables.xaml.cs)");
+                }
+            }
+            catch (FormatException)
+            {
+                dieFace.Text = "Error";
+            }
         }
 
         private void randomTablesTree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
             randomGeneratorModel.CurrentTable = randomTablesTree.SelectedValue.ToString();
         }
+
+        private void textChangedEventHandler(object sender, TextChangedEventArgs args)
+        {
+            if ((table.Count == 0) && (!firstInitialize))
+            {
+                for (int i = 0; i < randomTablesTree.Items.Count; i++)
+                {
+                    table.Add((RandomTableCategory)randomTablesTree.Items.GetItemAt(i));
+                }
+                firstInitialize = true;
+                randomTablesTree.Items.Clear();
+                randomTablesTree.ItemsSource = table;
+                for (int i = 0; i < table.Count; i++)
+                {
+                    x.Add((RandomTableCategory)table[i]);
+                }
+            }
+            else
+            {
+                randomTablesTree.ItemsSource = null;
+                table.Clear();
+                for (int i = 0; i < x.Count; i++)
+                {
+                    table.Add((RandomTableCategory)x[i]);
+                }
+                randomTablesTree.ItemsSource = table;
+                return;
+            }
+            List<RandomTableCategory> newTree = new List<RandomTableCategory>();
+            for (int i = 0; i < table.Count; i++)
+            {
+                var y = table[i];
+                int k = ((RandomTableCategory)y).SubCategories.Count;
+
+                if (k == 0)
+                {
+                    if (((RandomTableCategory)y).Name.Contains(SearchBox.Text))
+                    {
+                        Console.WriteLine(((RandomTableCategory)y).Name);
+                        newTree.Add((RandomTableCategory)y);
+                    }
+                }
+
+                for (int j = 0; j < k; j++)
+                {
+                    if (((RandomTableCategory)y).SubCategories[j].Name.Contains(SearchBox.Text))
+                    {
+                        Console.WriteLine(((RandomTableCategory)y).SubCategories[j].Name);
+                        newTree.Add(((RandomTableCategory)y).SubCategories[j]);
+                    }
+                }
+                
+            }
+            randomTablesTree.ItemsSource = null;
+            table.Clear();
+            for (int i = 0; i < newTree.Count; i++)
+            {
+                table.Add(newTree[i]);
+            }
+            randomTablesTree.ItemsSource = table;
+        }
+
     }
+
 }
